3b0a5248a70958f6763bd72417e1092f9c07c95c
unknown <1163751581@qq.com>
2018-03-28 12:30:32 +0800

    vuex

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="home">
 		<ul class="border-list">
-			<li class="list-box">
+			<li class="list-box" v-for="(item, i) in list">
 				<img class="list-img" src="../../assets/images/chat.png">
 				<div class="list-info">
-					<h4>hfoer</h4>
-					<p>424349</p>
+					<h4>{{item.id}}</h4>
+					<p>424349{{item.id}}</p>
 				</div>
 				<a class="btn-theme">点击</a>
 			</li>
@@ -13,15 +13,26 @@
 	</div>
 </template>
 <script>
-	export default {
-		name: 'home',
-		data() {
-			return {
-
-			}
+import {data} from '/api/api'
+export default {
+	name: 'home',
+	data() {
+		return {
+			list: []
 		}
-
+	},
+	mounted() {
+		this.initData();
+	},
+	methods: {
+		initData() {
+			data().then(res => {
+				this.list = res.list;
+			})
+		}
 	}
+
+}
 </script>
 <style lang="scss">
 	
